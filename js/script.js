9504c742c9b71aaa498e330d373e27cb983772d7
MohamedHajiAhamed <hajiahamed.2003@gmail.com>
2024-06-11 14:17:03 +0530

    feat:Updated UI

--- js/script.js
+++ js/script.js
@@ -3,28 +3,20 @@ const dark = document.querySelector('.fa-moon');
 const light = document.querySelector('.fa-sun');
 const to_top = document.querySelector('.to-top');
 const loader = document.querySelector('.loader');
-
-// Function to handle adding and removing the 'dark' class
 function toggleDarkMode() {
     body_dark.classList.toggle('dark');
     dark.classList.toggle('show');
     light.classList.toggle('show');
 }
-
 window.addEventListener('load', () => {
     loader.classList.add("disappear");
 });
-
-// Toggle dark mode when dark or light icons are clicked
 dark.addEventListener('click', () => {
     toggleDarkMode();
 });
-
 light.addEventListener('click', () => {
     toggleDarkMode();
 });
-
-// Toggle dark mode based on time (optional)
 /*
 const now = new Date();
 const hours = now.getHours();
@@ -32,8 +24,6 @@ if (hours >= 18 || hours <= 6) {
     toggleDarkMode();
 }
 */
-
-// Show/hide 'to-top' button based on scroll position
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
         to_top.classList.add('active');
@@ -41,14 +31,11 @@ window.addEventListener('scroll', () => {
         to_top.classList.remove('active');
     }
 });
-
-// Handle touch events for dark/light mode toggle
 dark.addEventListener('touchend', (event) => {
-    event.preventDefault(); // Prevent default touch behavior
+    event.preventDefault(); 
     toggleDarkMode();
 });
-
 light.addEventListener('touchend', (event) => {
-    event.preventDefault(); // Prevent default touch behavior
+    event.preventDefault(); 
     toggleDarkMode();
 });
